1bfae3520d35495a13af2eb44559175a9e480a01
Maxmillian Afanga <maxmillianafanga@gmail.com>
2021-04-15 21:20:30 +0400

    photo gallery

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlbumController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+// Route::get('/albums', [AlbumController::class,'index'])->name('albums.index')->middleware('auth');
+
+
+Route::resource('/albums', AlbumController::class)->middleware('auth');
+Route::post('albums/{album}/upload',[AlbumController::class,'upload'])->name('albums.upload')->middleware('auth');
